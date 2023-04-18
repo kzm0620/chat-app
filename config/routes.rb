f08dc251f594c378cc739123205a45a5d4cdb5fd
kzm0620 <kzm.staygold@gmail.com>
2023-04-18 17:49:54 +0900

    新規作成画面の表示

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   root to: "messages#index"
   resources :users, only: [:edit, :update]
   devise_for :users
+  resources :rooms, only: [:new, :create]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
 end
