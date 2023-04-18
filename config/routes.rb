20c6870d12ea5fe1d9798a2771ce722fe4c81f29
kzm0620 <kzm.staygold@gmail.com>
2023-04-18 16:32:00 +0900

    ユーザー編集機能の実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 Rails.application.routes.draw do
 
-  get 'messages/index'
   root to: "messages#index"
-
+  resources :users, only: [:edit, :update]
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
